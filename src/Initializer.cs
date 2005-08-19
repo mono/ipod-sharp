648ee58e31bca08c3f8c5d5ab7ef4a3f1ecfad0e
Aaron Bockover <abockover@novell.com>
2005-08-19 15:02:29 +0000

    Use new (threaded, default) or default context, capacities list tweaked
    svn path=/trunk/ipod-sharp/; revision=48559

--- src/Initializer.cs
+++ src/Initializer.cs
@@ -1,12 +1,13 @@
-
 using System;
 using System.Threading;
 using System.Runtime.InteropServices;
 
 namespace IPod {
 
-    internal class Initializer {
-        private static bool inited = false;
+    public class Initializer {
+        private static Initializer instance;
+		private static bool inited;
+		private static bool useDefaultContext;
 
         private static IntPtr mainContext;
 
@@ -15,6 +16,9 @@ namespace IPod {
         
         [DllImport ("libglib-2.0.so.0")]
         private static extern IntPtr g_main_context_new ();
+
+		[DllImport ("libglib-2.0.so.0")]
+		private static extern IntPtr g_main_context_default();
 
         [DllImport ("libglib-2.0.so.0")]
         private static extern void g_main_context_iteration (IntPtr ctx, bool mayblock);
@@ -28,28 +32,43 @@ namespace IPod {
         [DllImport ("libdbus-glib-1.so.1")]
         private static extern void dbus_g_thread_init ();
         
-        public static void Init () {
-            lock (typeof (Initializer)) {
-                if (inited)
-                    return;
-                
-                dbus_g_thread_init ();
-                g_type_init ();
-                GLib.GType.Register (Device.GType, typeof (Device));
-                GLib.GType.Register (DeviceEventListener.GType, typeof (DeviceEventListener));
-                
-                mainContext = g_main_context_new ();
-                ipod_device_set_global_main_context (mainContext);
-                
-                Thread mainThread = new Thread (new ThreadStart (MainLoopThread));
-                mainThread.IsBackground = true;
-                mainThread.Start ();
-
-                inited = true;
+        public static bool UseDefaultContext {
+            set {
+        	    useDefaultContext = value;
             }
         }
+        
+        public static void Init () {
+            lock (typeof (Initializer)) {
+                if(inited)
+                    return;
+           			
+                inited = true;
+                
+                if(!useDefaultContext) {
+                    dbus_g_thread_init ();
+                    g_type_init ();
+                }
+                
+                GLib.GType.Register(Device.GType, typeof(Device));
+                GLib.GType.Register(DeviceEventListener.GType, 
+                    typeof(DeviceEventListener));
+                
+                mainContext = useDefaultContext ? 
+                    g_main_context_default() :
+                    g_main_context_new();
+                	
+                ipod_device_set_global_main_context (mainContext);
+                
+                if(!useDefaultContext) {
+                    Thread mainThread = new Thread (new ThreadStart (MainLoopThread));
+                    mainThread.IsBackground = true;
+                    mainThread.Start ();
+                }
+			}
+        }
 
-        private static void MainLoopThread () {
+      private static void MainLoopThread () {
             while (true) {
                 try {
                     while (g_main_context_pending (mainContext))
